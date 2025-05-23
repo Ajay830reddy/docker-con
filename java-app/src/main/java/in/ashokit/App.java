b9582ca4ef913b70ea79e12e03abd3b5b81268c7
Ajay830reddy <gaddamajay42@gmail.com>
2025-05-23 20:28:57 +0530

    Update App.java

--- java-app/src/main/java/in/ashokit/App.java
+++ java-app/src/main/java/in/ashokit/App.java
@@ -1,13 +1,13 @@
 package in.ashokit;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class Application implements WebMvcConfigurer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
